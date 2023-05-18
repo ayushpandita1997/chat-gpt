227696e88bd8b8f1be85b806a2376f917c3344b6
Ayush Pandita <ayushpandita27@gmail.com>
2023-05-18 19:23:25 +0530

    updating dependencies

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { Conversation } from './gpt';
+
+function App() {
+  return (
+    <div className="App">
+     <Conversation />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/gpt/index.tsx
@@ -0,0 +1,11 @@
+import {FC} from "react";
+
+const Conversation = () => {
+    return(
+        <div>
+            {'Initial Commit'}
+        </div>
+    )
+}
+
+export { Conversation };
